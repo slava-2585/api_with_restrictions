0e2d9497d7e70f5493fca2a8712ce6059149a34b
Nikiforov Vyacheslav <slava-2585@yandex.ru>
2023-12-10 18:31:21 +0300

    initial commit

---
+++ advertisements/views.py
@@ -0,0 +1,26 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.throttling import AnonRateThrottle
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwnerOrReadOnly
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+
+    throttle_classes = [AnonRateThrottle]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated()]
+        return []
